3b986a1017e2c9e9aa46d56c1a427ee6d5a056e7
Artem Muz <LoGenMuz@gmail.com>
2025-09-29 06:59:31 +0300

    2.5 step 12

--- 2.5/12step.py
+++ 2.5/12step.py
@@ -1,16 +1,14 @@
 text = input()
 arr = text.split()
 arr.sort()
-arr += ' '
-num = 0
+conutDubleNumber = 0
 i = 0
 while i in range(len(arr)-1):
     while arr[i+1] == arr[i]:
-            num += 1
-            i += 1
+        conutDubleNumber += 1
+        i += 1
 
-    if num > 0:
-        num = 0 
+    if conutDubleNumber > 0:
         print(arr[i])
-        i+=1
     i +=1
+    conutDubleNumber = 0 
